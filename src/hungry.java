d67a1bb307c4a30c7e981b1950a375d827d039c5
YousefAbusabha <yousefabusabha17@yahoo.com>
2024-05-21 12:33:51 -0400

    second hunry commit

--- src/hungry.java
+++ src/hungry.java
@@ -5,6 +5,10 @@ public class hungry {
 		System.out.println("We are all hungry");
 		
 	}
+	
+	public static void step17() {
+		System.out.println("This is the second method of hungry class");
+	}
 }
 		
 		
